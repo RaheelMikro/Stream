8e8815c9cc2270d489537f88d18c305d9f265369
Raheel <raheel@mikrostartech.com>
2023-11-06 16:16:35 +0500

    www

--- Audio.py
+++ Audio.py
@@ -1,15 +1,8 @@
 import streamlit as st
 import time
-import numpy as np
 st.title("hello Welcome")
 col1,col2,col3=st.columns(3)
 with col1:
     st.header("Bat")
 with col2:
     st.header("Ball")
-with col3:
-        
-   
-
-
-
